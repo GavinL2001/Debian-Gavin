caee525e6029c9288863a44131743bd0941b1bb6
GavinL2001 <gavinliddell@hotmail.com>
2024-01-29 18:44:59 -0600

    fix fastfetch install, re-arrange some code for faster install, removed logging that doesn't work unless script finishes.

--- autosetup.sh
+++ autosetup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-((
 # Set username
 user=gavin
 
@@ -17,10 +16,13 @@ fi
 
 # Install basic packages
 apt update
-apt install -y sudo nala timeshift gpg
+apt install -y sudo nala timeshift gpg ufw flatpak
 
 #Add user to sudo group
 usermod -aG sudo gavin
+
+# Enable firewall
+ufw enable
 
 # Fix debian root directory subvolume name for timeshift backups
 sed -i 's+@rootfs+@+g' /etc/fstab
@@ -28,13 +30,49 @@ sed -i 's+@rootfs+@+g' /etc/fstab
 # Create initial back-up
 timeshift --btrfs --create --comments "after initial install"
 
-# Get faster mirrors
-nala fetch --auto --non-free --https-only -y
+# Fastfetch Install
+get_latest () { 
+    curl --silent "https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
+latest=$(get_latest)
+nala install -y https://github.com/fastfetch-cli/fastfetch/releases/download/$latest/fastfetch-$latest-Linux.deb
+
+# Flatpak Install
+run_as_user 'flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo'
+run_as_user 'flatpak install --user -y flathub \
+    com.bitwarden.desktop \
+    com.chatterino.chatterino \
+    com.discordapp.Discord \
+    com.github.tchx84.Flatseal \
+    com.spotify.Client \
+    com.usebottles.bottles \
+    com.vscodium.codium \
+    io.github.dweymouth.supersonic \
+    org.audacityteam.Audacity \
+    org.avidemux.Avidemux \
+    org.bleachbit.BleachBit \
+    org.gimp.GIMP \
+    org.telegram.desktop \
+    us.zoom.Zoom \
+    org.libreoffice.LibreOffice \
+    one.ablaze.floorp \
+    net.davidotek.pupgui2'
+
+# Create second back-up
+timeshift --btrfs --create --comments "pre-sid upgrade"
 
 # Add Xanmod repository
 curl -s https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+
+# Get sid mirrors
+nala fetch --auto --non-free --https-only --debian sid -y
+
+# Updating Existing Packages
 nala update
+nala upgrade -y
 
 # Installing New Packages
 nala install -y \
@@ -72,7 +110,6 @@ nala install -y \
     steam-installer \
     tldr \
     trash-cli \
-    ufw \
     wget \
     winetricks \
     wireguard \
@@ -83,48 +120,7 @@ nala install -y \
 # Clean up leftover cache
 nala clean
 
-# Fastfetch Install
-get_latest () { 
-    curl --silent "https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest" |
-    grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/'
-}
-latest=$(get_latest)
-nala install https://github.com/fastfetch-cli/fastfetch/releases/download/$latest/fastfetch-$latest-Linux.deb
-
-# Flatpak Install
-run_as_user 'flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo'
-run_as_user 'flatpak install --user -y flathub \
-    com.bitwarden.desktop \
-    com.chatterino.chatterino \
-    com.discordapp.Discord \
-    com.github.tchx84.Flatseal \
-    com.spotify.Client \
-    com.usebottles.bottles \
-    com.vscodium.codium \
-    io.github.dweymouth.supersonic \
-    org.audacityteam.Audacity \
-    org.avidemux.Avidemux \
-    org.bleachbit.BleachBit \
-    org.gimp.GIMP \
-    org.telegram.desktop \
-    us.zoom.Zoom \
-    org.libreoffice.LibreOffice \
-    one.ablaze.floorp \
-    net.davidotek.pupgui2'
-
-# Create second back-up
-timeshift --btrfs --create --comments "pre-sid upgrade"
-
-# Get sid mirrors
-nala fetch --auto --non-free --https-only --debian sid -y
-
-# Updating Existing Packages
-nala update
-nala upgrade -y
-
 # Post-install Things
-ufw enable
 run_as_user 'chsh -s $(which zsh)'
 
 # Create post-install back-up
@@ -132,5 +128,3 @@ timeshift --btrfs --create --comments "after installation"
 
 printf "Initial setup completed!\nPlease install Hyprland using this script here:\nhttps://github.com/JaKooLit/Debian-Hyprland\n"
 printf "Log file saved at /home/$user/debian-installer.log\n"
-
-) 3>&1) | tee -a /home/$user/debian-installer.log
