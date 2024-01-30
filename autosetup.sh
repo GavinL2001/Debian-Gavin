d4dc901fb9f887f3154d566039a3dafc4462c748
GavinL2001 <gavinliddell@hotmail.com>
2024-01-29 20:51:19 -0600

    re-add --if-not-exists flag

--- autosetup.sh
+++ autosetup.sh
@@ -30,7 +30,7 @@ sed -i 's+@rootfs+@+g' /etc/fstab
 timeshift --btrfs --create --comments "after initial install"
 
 # Flatpak Install
-flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sleep 5
 sudo su -l $user -c '
 flatpak install --user -y \
